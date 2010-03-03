1d0217a595c12d83bcbd2d8227b1f85e117b65a3
Domenico Rotiroti <drotiro@tiscali.it>
2010-03-03 17:37:21 +0100

    Doc update

--- utils/postpic_utils.c
+++ utils/postpic_utils.c
@@ -10,6 +10,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <getopt.h>
+#include <termios.h>
 
 void pp_print_usage(const char *pname)
 {
@@ -38,6 +39,34 @@ void pp_parse_connect_opt(char c, pp_connect_options * opts) {
 		}
 }
 
+/*
+ * askpass and set_echo are 'stolen' from boxfs,
+ * I guess I can do it ;)
+ */
+void set_echo(int enable)
+{
+  struct termios tio;
+  int tty = fileno(stdin); //a better way?
+
+  if(!tcgetattr(tty, &tio)) {
+    if (enable) tio.c_lflag |= ECHO;
+    else tio.c_lflag &= ~ECHO;
+
+    tcsetattr(tty, TCSANOW, &tio);
+  }
+}
+
+char * askpass(const char * what)
+{
+  char * val = malloc(512);
+  printf("%s ",what);
+  set_echo(0);
+  scanf("%s",val);
+  set_echo(1); 
+  printf("\n");
+  return val;
+}
+
 #define PP_ADDPROP(NAME) if(opts->NAME) { strcat(buf, " " #NAME " = "); strcat(buf,opts->NAME); }
 PGconn * pp_connect(pp_connect_options * opts)
 {
@@ -45,6 +74,7 @@ PGconn * pp_connect(pp_connect_options * opts)
 	PP_ADDPROP(host);
 	PP_ADDPROP(dbname);
 	PP_ADDPROP(user);
+	if(!opts->password) opts->password = askpass("password: ");
 	PP_ADDPROP(password);
 	
 	return PQconnectdb(buf);
